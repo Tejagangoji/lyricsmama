0fb9d0950db513641a4ea89752ebb5fe2fd6a670
Tejagangoji <124460877+Tejagangoji@users.noreply.github.com>
2023-06-13 14:37:18 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,19 @@ export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Layout>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+         />
+
+        <Script id="google-analytics-script" strategy="lazyOnload">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+          });`}
+        </Script>
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
